a4f66070ee4d717022ff3cfc806d1a466c9e9ba7
Aviral <adhikariaviral11@gmail.com>
2026-05-05 13:48:06 +0545

    Half Adder and Full adder completed

---
+++ code/verilog/Day3/full_adder_tb.v
@@ -0,0 +1,27 @@
+module full_adder_tb;
+reg a;
+reg b;
+reg cin;
+wire sum;
+wire cout;
+
+full_adder full_adder_uut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+);
+
+initial begin
+a=0; b=0; cin=0; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=0; b=0; cin=1; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=0; b=1; cin=0; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=0; b=1; cin=1; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=1; b=0; cin=0; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=1; b=0; cin=1; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=1; b=1; cin=0; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+a=1; b=1; cin=1; #10; $display("a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+$finish;
+end
+endmodule
