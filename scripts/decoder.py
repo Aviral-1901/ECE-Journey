668a5d22d8ed88a73cf5915d8ff9d1871e680159
Aviral <adhikariaviral11@gmail.com>
2026-06-17 19:26:22 +0545

    feat: add decoder for B and J type instructions

--- scripts/decoder.py
+++ scripts/decoder.py
@@ -65,3 +65,49 @@ elif (opcode==0x23):
 
     if (funct3==2):
         print(f"sw x{rs2}, {imm_signed}(x{rs1})")
+
+
+elif (opcode==0x63):
+    imm11 = (hex_value >> 7) & 0x1
+    imm4_1 = (hex_value >> 8) & 0xF
+    funct3 = (hex_value >> 12) & 0x7
+    rs1 = (hex_value >> 15) & 0x1F
+    rs2 = (hex_value >> 20) & 0x1F
+    imm10_5 = (hex_value >> 25) & 0x3F
+    imm12 = (hex_value >> 31) & 0x1
+    immediate = (imm12 << 12) | (imm11 << 11) | (imm10_5 << 5) | (imm4_1 << 1) | 0
+
+    print("imm4_1 : ",imm4_1)
+    print("imm10_5 : ",imm10_5)
+    print("imm11 : ",imm11)
+    print("imm12 : ",imm12)
+
+    if immediate & 0x1000: #checking for bit12 (signbit)
+        immediate -= 0x2000 #subtracting by the number which has 1 in bit13
+
+    if (funct3==0):
+        print(f"beq x{rs1}, x{rs2}, {immediate}")
+    
+    elif (funct3==1):
+        print(f"bne x{rs1}, x{rs2}, {immediate}")
+
+    elif (funct3==4):
+        print(f"blt x{rs1}, x{rs2}, {immediate}")
+
+    elif (funct3==5):
+        print(f"bge x{rs1}, x{rs2}, {immediate}")
+
+
+elif (opcode==0x6F):
+    rd = (hex_value >> 7) & 0x1F
+    imm19_12 = (hex_value >> 12) & 0xFF
+    imm11 = (hex_value >> 20) & 0x1
+    imm10_1 = (hex_value >> 21) & 0x3FF
+    imm20 = (hex_value >> 31) & 0x1
+
+    offset = (imm20 << 20) | (imm19_12 << 12) | (imm11 << 11) | (imm10_1 << 1) | 0
+
+    if offset & 0x100000:
+        offset -= 0x200000
+
+    print(f"jal x{rd}, {offset}")
