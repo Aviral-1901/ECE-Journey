f222596cb404ff7132002339eca0ad4e5a0041a1
Aviral <adhikariaviral11@gmail.com>
2026-07-07 00:47:17 +0545

    feat: add I-type immediate extraction to imm_gen and resolved icarus syntax issues

--- rtl/cpu/imm_gen.sv
+++ rtl/cpu/imm_gen.sv
@@ -3,18 +3,33 @@ module imm_gen(
     output logic [31:0] imm
 );
 
+logic [6:0] opcode;
+assign opcode = instruction[6:0];
+
+logic [31:0] i_type_imm;
+logic [31:0] s_type_imm;
+logic [31:0] b_type_imm;
+
+assign i_type_imm = {{20{instruction[31]}}, instruction[31:20]};
+assign s_type_imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+assign b_type_imm = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+
 always_comb begin
-    case (instruction[6:0])
-        7'b0000011: begin
-            imm = {{20{instruction[31]}}, instruction[31:20]};
+    case (opcode)
+        7'b0000011: begin //lw
+            imm = i_type_imm;
         end
 
-        7'b0100011: begin
-            imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+        7'b0010011: begin // I-Type ALU (addi, etc.)
+            imm = i_type_imm;
         end
 
-        7'b1100011: begin
-            imm = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+        7'b0100011: begin //sw
+            imm = s_type_imm;
+        end
+
+        7'b1100011: begin //b-type
+            imm = b_type_imm;
         end
 
         default: begin
