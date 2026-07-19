fd758adc5ad57b83f4c23ae41ca1aea858f2b961
Aviral <adhikariaviral11@gmail.com>
2026-07-19 14:01:02 +0545

    feat: add J-type immediate extraction for JAL

--- rtl/cpu/imm_gen.sv
+++ rtl/cpu/imm_gen.sv
@@ -9,10 +9,12 @@ assign opcode = instruction[6:0];
 logic [31:0] i_type_imm;
 logic [31:0] s_type_imm;
 logic [31:0] b_type_imm;
+logic [31:0] j_type_imm;
 
 assign i_type_imm = {{20{instruction[31]}}, instruction[31:20]};
 assign s_type_imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
 assign b_type_imm = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+assign j_type_imm = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
 
 always_comb begin
     case (opcode)
@@ -30,6 +32,10 @@ always_comb begin
 
         7'b1100011: begin //b-type
             imm = b_type_imm;
+        end
+        
+        7'b1101111: begin //j-type
+            imm = j_type_imm;
         end
 
         default: begin
